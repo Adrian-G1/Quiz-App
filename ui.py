29a85b2efdebceac1f001cea2e61ded6f21671c2
Adrian Garcia <agarciacarillo@gmail.com>
2023-02-14 22:39:15 -0600

    implemented tkinter ui

--- ui.py
+++ ui.py
@@ -1,2 +1,37 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizlet")
+        self.window.config(background=THEME_COLOR, padx=20, pady=20)
+
+        # Set Score Label
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        # Set Canvas
+        self.canvas = Canvas(height=250, width=300)
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        # Set Buttons
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_img)
+        self.true_button.grid(column=0, row=2)
+
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_img)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
