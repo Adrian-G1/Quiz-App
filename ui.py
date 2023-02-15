62a9a094e011e7c5044809c9532d88ac709a22d5
Adrian Garcia <agarciacarillo@gmail.com>
2023-02-15 09:37:38 -0600

    questions displayed in ui

--- ui.py
+++ ui.py
@@ -1,11 +1,14 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizlet")
         self.window.config(background=THEME_COLOR, padx=20, pady=20)
@@ -19,6 +22,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="Some Question Text",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
@@ -34,4 +38,11 @@ class QuizInterface:
         self.false_button = Button(image=false_img)
         self.false_button.grid(column=1, row=2)
 
+        # Display question
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
